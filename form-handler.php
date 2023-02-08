18ba3cf5a2753ac5eeb38513ea14166582e8e1db
jubilant-github <122648651+jubilant-github@users.noreply.github.com>
2023-02-08 22:23:02 +0200

    php correct

--- form-handler.php
+++ form-handler.php
@@ -9,13 +9,13 @@ $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
                 "Subject: $subject.\n".
-                "User Message: $message";
+                "User Message: $message .\n";
 
 $to = 'lorraine.somelane@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
-$headers = "Reply-To: $visitor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
